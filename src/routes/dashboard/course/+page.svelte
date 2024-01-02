90e018f5c6bf899f46a37ab5eb5cc4c14fcd9bf7
Julian Hammer <jh@moinjulian.com>
2024-01-02 17:01:58 +0100

    Information about course

--- src/routes/dashboard/course/+page.svelte
+++ src/routes/dashboard/course/+page.svelte
@@ -1,2 +1,17 @@
 <h1>Course</h1>
 
+<div class="information">
+    <p>Please put in some information about the course your playing:</p>
+
+    <label for="course_name">Course Name:</label>
+    <input type="text" name="course_name" id="course_name">
+
+    <label for="course_location">Course Location</label>
+    <input type="text" name="course_location" id="course_name">
+
+    <label for="par_info">Par Info:</label>
+    <input type="number" name="par_info" id="par_info">
+
+    <label for="course_rating">Course Rating</label>
+    <input type="number" name="course_rating" id="course_rating">
+</div>
